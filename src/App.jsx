8f0a40dbfc02eb77cf3bb5c49d754f1a8d7aa133
Edith Ruiz <edith.ruiz300894@gmail.com>
2022-07-19 20:59:12 -0500

    Se implemento la libreria para traducir los textos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import './App.css'
 import Navbar from './componente/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
+  const {t}=useTranslation();
 
   return (
     <div className="App">
       <Navbar/>
-      <div>Hola</div>
+      <div>
+        <div>
+          
+        </div>
+        <div> 
+          <h3 >{t("Tecnologias")}</h3>
+          <img src='public\img.PNG'></img>
+        </div>
+      </div>
       
     </div>
   )
--- src/componente/Navbar.jsx
+++ src/componente/Navbar.jsx
@@ -1,11 +1,18 @@
+import { useTranslation } from "react-i18next";
 
 const Navbar = () => {
+
+    const { i18n } = useTranslation();
+
+    function changeLanguage(lenguaje) {
+        i18n.changeLanguage(lenguaje);
+    }
 
     return (
         <nav>
             <div><a>Home</a></div>
-            <div><a onClick={()=> cambiarLenguaje("en")}>Inglés</a></div>
-            <div><a onClick={()=> cambiarLenguaje("es")}> Español</a></div>
+            <a onClick={() => changeLanguage("en")}>Inglés</a>
+            <a onClick={() => changeLanguage("es")}> Español</a>
         </nav>
     );
 };
